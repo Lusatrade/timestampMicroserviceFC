5947cdd3ba56d3edaa5581fa05db8c32053d3872
Lusatrade <lusatrade@gmail.com>
2022-06-30 14:46:54 +0200

    Final commit

--- index.js
+++ index.js
@@ -5,6 +5,9 @@
 var express = require('express');
 var app = express();
 
+const dateAndTime =require("date-and-time")
+
+
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
@@ -25,7 +28,35 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+const parseDateTime = (dt)=>{
+  const strFormat = "ddd, DD MMM YYYY HH:mm:ss [UTC]"
+  
+  try {
+    const date = new Date(dt)
+    if(!date.getTime()||undefined ===date.getTime()){
+      throw new Error('Invalid date')
+    }
+    const fmt = dateAndTime.format(date,strFormat,true)
+    return{
+      unix: date.getTime(),
+      utc: fmt
+    }
 
+  } catch (error) {
+    return {error: "Invalid dateTime parameter",message:error}
+  }
+}
+  parseDateTime('2015-12-32')
+
+app.get('/api/:date_time',(req,res)=>{
+  const result = parseDateTime(req.params.date_time)
+  if(result.error){
+    res.status(400).json({error:"Invalid request type"})
+  }else{
+    res.status(200).json(result)
+  }
+ 
+})
 
 // listen for requests :)
 var listener = app.listen(PORT, function () {
